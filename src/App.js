23315fab8463680fd568ed580256ebfe5b39ac02
jun.favila <jun.favila@ntt.com.ph>
2024-02-02 09:12:26 +0800

    add react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import React from 'react';
+import CenteredBox from './CenteredBox';
+
+
+
+function App() {
+
+
+
+
+  
+  return (
+    <div className="App">
+
+      <CenteredBox />
+   
+    </div>
+  );
+}
+
+export default App;
